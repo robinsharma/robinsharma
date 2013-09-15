8f7f5f3743bbbe9254a341baae0224cfc5cfb357
Robin Sharma <sharms115@gmail.com>
2013-09-15 02:24:28 -0400

    Update web.js

--- web.js
+++ web.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var fs = require('fs');
 var htmlfile = "index.html";
-var http = require("http");
 
 var app = express();
 
@@ -11,6 +10,6 @@ app.get('/', function(request, response) {
     });
 
 var port = process.envPORT || 8080;
-http.createServer(app).listen(port, function() {
-	console.log("Listen on " + port + "!");
+app.listen(port, function() {
+	console.log("Listening on " + port);
     });
