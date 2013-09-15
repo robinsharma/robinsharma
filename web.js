8681da0fbe23de36b463eb91c32395572ce8dc6b
Robin Sharma <sharms115@gmail.com>
2013-09-15 04:55:23 -0400

    Deleted extra style.css, moved resume.pdf to root

--- web.js
+++ web.js
@@ -9,6 +9,7 @@ app.get('/', function(request, response) {
 	response.send(html);
     });
 app.use("/css", express.static(__dirname + '/css'));
+app.use("/", express.static(__dirname + '/'));
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log("Listening on " + port);
