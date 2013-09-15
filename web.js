5fec2e4fdbd6e8c2654de87f0d820bd77ec4a21a
Robin Sharma <sharms115@gmail.com>
2013-09-15 01:32:39 -0400

    Setting up node, index.html

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
+
+var app = express();
+
+app.get('/', function(request, response) {
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
+    });
+
+var port = process.envPORT || 8080;
+app.listen(port, function() {
+	console.log("Listen on " + port + "!");
+    });
